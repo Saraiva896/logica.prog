77fe42e2097d6f60ab705a4b00a9907c69c2ab06
saraiva <joaosaraiva896@gmail.com>
2026-06-17 09:19:41 -0300

    teste

---
+++ l041q02.js
@@ -0,0 +1,13 @@
+/* Desenvolver um programa que permita ao aluno responder qual a capital do Brasil. O programa deverá exibir se
+a resposta está certa ou errada.*/
+let capital; 
+
+console.log("Qual a capital do BRASIL:");
+
+if(capital || BRASIL){
+    capital = brasília;
+    console.log(`CORRETO! A CAPITAL DE BRASIL É ${capital}`);
+}else{
+    console.log(`ERRADA! A CAPITAL DO BRASIL NÃO É ${capital}`);
+}
+
---
+++ l041q03.js
@@ -0,0 +1,13 @@
+let num, resto;
+
+console.log("informe um numero: ");
+num = 11;
+console.log(`Valor inserido: ${num}`);
+
+resto = num % 2;
+
+if (resto === 0 ){
+    console.log(`${num} é par`);
+}else{
+    console.log(`${num} é impar` );
+}
