4112e5d95c854b12aa7b0fcecf9bb4246a5206ae
saraiva <joaosaraiva896@gmail.com>
2026-06-19 10:20:06 -0300

    for

--- l053q2.js
+++ l053q2.js
@@ -1,10 +1,13 @@
 /* Desenvolver um programa que apresente o total da soma de n números inteiros do número 1 até n, onde n é um
 valor informado pelo usuário.
-*/
-let n = 3;
+*/ 
+
+console.log( "informe um numero:");
+let n = 20;
 let acum, soma;
 
-for (let cont = 1; cont <= 20; cont = cont + 1){
+console.log(`numero inserido: ${n}`); 
+for (let cont = 1; cont <= n; cont = cont + 1){
     acum = n + cont;
     soma = cont + acum;
 }
